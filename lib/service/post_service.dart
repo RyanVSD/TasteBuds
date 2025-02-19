0488346561a73dab79eca87dcbce7df49bc22a70
Junxi <jasonwu700@outlook.com>
2025-02-19 10:43:04 -0800

    Linked homepage to posts

--- lib/service/post_service.dart
+++ lib/service/post_service.dart
@@ -1,34 +1,19 @@
 
 import 'package:tastebuds/model/objects/post.dart';
+import 'package:tastebuds/model/dummy_data.dart';
 import 'dart:convert';
+
+String tempGetDummyJson(String postId) {
+  int id = int.parse(postId);
+  if (id >= dummyPosts.length || id < 0) return "{}";
+  return jsonEncode(dummyPosts[int.parse(postId)]);
+}
 
 // or return a map or something like this
 Future<Post?> getPost(String postId) async {
-
-    // Example JSON data
-  String jsonData = '''
-  {
-    "id": "1",
-    "title": "Fruit Smoothie",
-    "author": "Smoothie master",
-    "imageUrl": "https://static.wikia.nocookie.net/fruits-information/images/2/2b/Apple.jpg/revision/latest/thumbnail/width/360/height/450?cb=20180802112257",
-    "ingredients": ["1 apple", "4 orange", "3 banana"],
-    "steps": [
-      "1. put one apple",
-      "2. put four orange",
-      "3. put three banana",
-      "4. blend"
-    ],
-    "ratings": {
-      "difficulty": 1,
-      "price": 2,
-      "taste": 4
-    }
-  }
-  ''';
-
+  
+  String jsonData = tempGetDummyJson(postId);
   Map<String, dynamic> jsonMap = json.decode(jsonData);
-  Post post = Post.fromJson(jsonMap);
-  return post;
+  return jsonMap.isEmpty ?  Post.empty() : Post.fromJson(jsonMap);
 }
 
