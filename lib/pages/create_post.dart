57d1af0032f391d2b63decf6f32a6ea62fa397c8
Junxi <jasonwu700@outlook.com>
2025-02-23 22:56:28 -0800

    sync with main

--- lib/pages/create_post.dart
+++ lib/pages/create_post.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tastebuds/widgets/bottom_nav_bar.dart';
 
 class CreatePost extends StatefulWidget {
   const CreatePost({super.key});
@@ -12,7 +11,6 @@ class _CreatePostState extends State<CreatePost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavBar(),
         body: Center(child: Text("I love lebron")));
   }
 }
