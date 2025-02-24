90bdb3c2956f8b5fba4cc95644138e1de2297257
Junxi <jasonwu700@outlook.com>
2025-02-23 21:12:43 -0800

    Organized files

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tastebuds/model/post_model.dart';
-import './post_page.dart';
 import 'package:tastebuds/model/dummy_data.dart';
 import 'package:tastebuds/model/objects/post.dart';
-
+import './widget/post_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -96,88 +93,3 @@ class PostGrid extends StatelessWidget {
   }
 }
 
-class CardWidget extends StatelessWidget {
-  const CardWidget({
-    super.key,
-    required this.post,
-  });
-
-  final Post post;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        context.read<PostModel>().setPost(postId: post.id);
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => PostPage()));
-      },
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Container(
-              height: 120,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(post.imageUrl),
-                  fit: BoxFit.cover,
-                  alignment: Alignment.center,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    post.title,
-                    style: const TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 2),
-                  Text(
-                    'By ${post.author}',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.thumb_up,
-                              size: 16, color: Colors.blue),
-                          const SizedBox(width: 4),
-                          Text('${post.likes} Likes'),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Icon(Icons.favorite,
-                              size: 16, color: Colors.red),
-                          const SizedBox(width: 4),
-                          Text('${post.favorites} Favorites'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
