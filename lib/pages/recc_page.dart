70fbc2863e420ff3281adfd233b5c355a95fa7e4
Junxi <jasonwu700@outlook.com>
2025-02-23 22:41:51 -0800

    Recc page ui

--- lib/pages/recc_page.dart
+++ lib/pages/recc_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './widget/recc_card.dart';
+import 'package:tastebuds/model/dummy_data.dart';
 
 class ReccPage extends StatelessWidget {
   const ReccPage({super.key});
@@ -6,8 +8,27 @@ class ReccPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Text("Reccomendations"),
-        ));
+      appBar: AppBar(
+        backgroundColor: Colors.yellow[700],
+        title: Text(
+              "Recommendation",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w800
+              ),
+            ),
+      ),
+      body: ListView(
+        children: [
+          SizedBox(height: 4),
+          ReccRow(title: "For you", posts: dummyPosts),
+          ReccRow(title: "Nearest", posts: dummyPosts),
+          ReccRow(title: "Training diet", posts: dummyPosts),
+          ReccRow(title: "Chinese", posts: dummyPosts),
+          ReccRow(title: "Japanese", posts: dummyPosts),
+        ],
+      )
+    );
+    // return Placeholder();
   }
 }
---
+++ lib/pages/widget/recc_card.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:tastebuds/model/post_model.dart';
+import 'package:tastebuds/model/objects/post.dart';
+import 'package:provider/provider.dart';
+import '../post_page.dart';
+
+class ReccRow extends StatelessWidget {
+  final List<Post> posts;
+  final String title;
+
+  const ReccRow({super.key, required this.title, required this.posts});
+
+  @override
+  Widget build(BuildContext context) {
+    const double cardWidth = 170;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          SizedBox(
+            height: 180,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                SizedBox(width: 8,),
+                  ...List.generate(5, (index) {
+                  Post post = posts[index];
+                  return SizedBox(
+                    width: cardWidth, // Ensures 2 columns
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 1),
+                      child: ReccCard(post: post),
+                    ),
+                  );
+                }),
+                SizedBox(width: 8,),
+              ]
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class ReccCard extends StatelessWidget {
+  const ReccCard({
+    super.key,
+    required this.post,
+  });
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read<PostModel>().setPost(postId: post.id);
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => PostPage()));
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              // width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(post.imageUrl),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    post.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      // fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
