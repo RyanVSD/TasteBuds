e447578cb394633c0c3838ac11c19baa47ddee61
RyanVirtue619 <113137954+RyanVirtue619@users.noreply.github.com>
2025-03-05 17:47:56 -0800

    t

--- lib/pages/recc_page.dart
+++ lib/pages/recc_page.dart
@@ -10,29 +10,45 @@ class ReccPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        scrolledUnderElevation: 0,
-        backgroundColor:Theme.of(context).brightness == Brightness.dark ? Theme.of(context).colorScheme.surface : Theme.of(context).colorScheme.secondary,
-        title: Text(
-              "Recommendation",
-              style: TextStyle(
-                fontSize: 24,
-              ),
+        appBar: AppBar(
+          scrolledUnderElevation: 0,
+          backgroundColor: Theme.of(context).brightness == Brightness.dark
+              ? Theme.of(context).colorScheme.surface
+              : Theme.of(context).colorScheme.secondary,
+          title: Text(
+            "Recommendation",
+            style: TextStyle(
+              fontSize: 24,
             ),
-      ),
-      body: ListView(
-        children: [
-          SizedBox(height: 4),
-          // ReccRow(title: "For you", posts: context.watch<PostModel>().getPostList(5)),
-          // ReccRow(title: "Nearest", posts: dummyPosts),
-          ReccRow(title: "🥬 Vegeterian", posts: context.watch<PostModel>().getPostListContaining("vege")),
-          ReccRow(title: "🍕 Italian", posts: context.watch<PostModel>().getPostListContaining("italian")),
-          ReccRow(title: "🏋️ Training diet", posts: context.watch<PostModel>().getPostListContaining("training")),
-          ReccRow(title: "🥡 Chinese", posts: context.watch<PostModel>().getPostListContaining("chinese")),
-
-        ],
-      )
-    );
+          ),
+        ),
+        body: ListView(
+          children: [
+            SizedBox(height: 4),
+            // ReccRow(title: "For you", posts: context.watch<PostModel>().getPostList(5)),
+            // ReccRow(title: "Nearest", posts: dummyPosts),
+            ReccRow(
+                title: "🥬 Vegeterian",
+                posts: context
+                    .watch<PostModel>()
+                    .getPostListContaining("vegetarian")),
+            ReccRow(
+                title: "🍕 Italian",
+                posts: context
+                    .watch<PostModel>()
+                    .getPostListContaining("italian")),
+            ReccRow(
+                title: "🏋️ Training diet",
+                posts: context
+                    .watch<PostModel>()
+                    .getPostListContaining("training")),
+            ReccRow(
+                title: "🥡 Chinese",
+                posts: context
+                    .watch<PostModel>()
+                    .getPostListContaining("chinese")),
+          ],
+        ));
     // return Placeholder();
   }
 }
